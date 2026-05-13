b4978c6a54f9e99197b77362fbf831700069eb7c
manuel-ale-sistem <mmendezarizmendi02@gmail.com>
2026-05-13 13:18:35 -0600

    Update registro_services.py

--- services/registro_services.py
+++ services/registro_services.py
@@ -14,8 +14,13 @@ COLUMNAS = [
 
 
 # ---------- CONEXIÓN ----------
+import os
+
 def get_connection():
-    return sqlite3.connect(DB_PATH, check_same_thread=False)
+    # Crear carpeta si no existe
+    os.makedirs("/data", exist_ok=True)
+
+    return sqlite3.connect("/data/registro.db", check_same_thread=False)
 
 # ---------- INICIALIZAR BD ----------
 def inicializar_bd():
